7e4aeda86d93a92224182a1a86b4271c0142603a
Fernando Daciuk <f.daciuk@gmail.com>
2015-12-17 14:51:57 -0200

    Add http server

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 'use strict';
 
-var phantom = require('./lib/phantom'),
-	io = require('socket.io')(3500);
+var http = require('http'),
+	express = require('express'),
+	app = express(),
+	port = process.env.PORT || 5000,
+	server = http.createServer(app),
+	io = require('socket.io')(server),
+	phantom = require('./lib/phantom');
+
+server.listen(port);
 
 io.on('connect', socket => {
 	console.log('Socket.io on Phantom is connected!');
